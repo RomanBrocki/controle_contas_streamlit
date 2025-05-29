20d8d305189bccdd1865ab34191f8e9250663c47
RomanBrocki <romanbrocki@hotmail.com>
2025-05-29 18:57:23 -0300

    correçao de bug de persistência de histórico com troca de páginas e alteração do apresentado após acessar mês vigente.

--- interface/navegacao.py
+++ interface/navegacao.py
@@ -12,14 +12,18 @@ def ir_para_mes_vigente():
     """Define a tela atual como 'mes_vigente'."""
     st.session_state["tela_atual"] = "mes_vigente"
     st.session_state["modo_nova_conta"] = False
-
+    st.session_state["df_original"] = None
 
 def ir_para_historico():
     """Define a tela atual como 'historico'."""
     st.session_state["tela_atual"] = "historico"
     st.session_state["modo_nova_conta"] = False
-
+    st.session_state["df_original"] = None
+    st.session_state["historico_carregado"] = False
 
 def voltar_tela_inicial():
     """Retorna à tela inicial do aplicativo."""
     st.session_state["tela_atual"] = "inicial"
+    st.session_state["modo_nova_conta"] = False
+    st.session_state["df_original"] = None
+    st.session_state["historico_carregado"] = False
